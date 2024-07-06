538a12c30a46477f57abe29480d789ec91537d40
grausamkeitkkj <diogorschultz2@gmail.com>
2024-07-06 03:35:21 -0300

    Deixando o programa mais customizável

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
 from pathlib import Path
 
-x = Path(r'C:\Users\User')
-s = Path(r'C:\Users\User\Downloads')
+def organizar():
+    p = Path(input('Informe o caminho da pasta a ser organizada: '))
+    diretorio_base = Path(input(r'Informe o caminho diretório base (Pasta anterior a que será organizada): '))
+    print('Informe o nome da pasta a partir do diretório base')
+    diretorio_imagens = diretorio_base.joinpath(input('Informe o caminho da pasta de imagens: '))
+    diretorio_videos = diretorio_base.joinpath(input('Informe o caminho da pasta de vídeos: '))
+    diretorio_musica = diretorio_base.joinpath(input('Informe o caminho da pasta de música: '))
+    diretorio_documentos = diretorio_base.joinpath(input('Informe o caminho da pasta de documentos: '))
+    diretorio_downloads = diretorio_base.joinpath(input('Informe o caminho da pasta de downloads: '))
+    diretorio_executaveis = diretorio_base.joinpath(input('Informe o caminho da pasta de executáveis: '))
 
-extencoes ={
-    'pdf': 'Documents',
-    'txt': 'Documents',
-    'doc': 'Documents',
-    'docx': 'Documents',
-    'xls': 'Documents',
-    'xlsx': 'Documents',
-    'ppt': 'Documents',
-    'pptx': 'Documents',
-    'jpg': 'Pictures',
-    'jpeg': 'Pictures',
-    'png': 'Pictures',
-    'gif': 'Pictures',
-    'mp4': 'Videos',
-    'mp3': 'Music',
-    'zip': 'Downloads',
-    'rar': 'Downloads',
-    'exe': 'Downloads',
-}
+    extencoes ={
+        'pdf': diretorio_documentos,
+        'txt': diretorio_documentos,
+        'doc': diretorio_documentos,
+        'docx':diretorio_documentos,
+        'xls': diretorio_documentos,
+        'xlsx':diretorio_documentos,
+        'ppt': diretorio_documentos,
+        'pptx':diretorio_documentos,
+        'jpg': diretorio_imagens,
+        'jpeg':diretorio_imagens,
+        'png': diretorio_imagens,
+        'gif': diretorio_imagens,
+        'mp4': diretorio_videos,
+        'mp3': diretorio_musica,
+        'zip': diretorio_downloads,
+        'rar': diretorio_downloads,
+        'exe': diretorio_executaveis,
+    }
 
-def organize():
-    diretorio_base = input('Informe o diretório base: ')
-    p = input('Informe o diretório a ser organizado: ')
-    diretorio_base = Path(diretorio_base) 
-    p = Path(p) 
-    for file in p.iterdir():
-        if file.is_file():
-            extencao = file.suffix[1:].lower()
+    for arquivos in p.iterdir():
+        if arquivos.is_file():
+            extencao = arquivos.suffix[1:].lower()
             if extencao in extencoes:
-                diretorio_alvo= diretorio_base / extencoes[extencao]
-                file.rename(diretorio_alvo / file.name)
+                try:
+                    diretorio_alvo= diretorio_base / extencoes[extencao]
+                    arquivos.rename(diretorio_alvo / arquivos.name)
+                except FileNotFoundError:
+                    diretorio_alvo.mkdir()
+                    arquivos.rename(diretorio_alvo / arquivos.name)
 
-organize()
+if __name__ == "__main__":
+    organizar()
